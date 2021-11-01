4b3b97c7003747e8c7e80b883c27809182e44784
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-11-01 15:55:12 -0300

    add kafka

--- back/src/controller/OrderController.js
+++ back/src/controller/OrderController.js
@@ -15,11 +15,9 @@ class OrderController {
 
     async closeOrder(req, res){
         try {
-            // const { producer } = req;
+            const { producer } = req;
             const { id, payment } = req.body;
-            const service = await orderService.closeOrder(id, payment,
-                // producer
-                );
+            const service = await orderService.closeOrder(id, payment, producer);
             res.status(200).json(service);
         }catch (err){
             res.status(400).json({ "error": err.message });
