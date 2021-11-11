fbb9fd16b9e521ca5306ab1b211ce5ea689c3ea3
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-11-11 09:12:34 -0300

    Coment header

--- front/src/components/Header/index.jsx
+++ front/src/components/Header/index.jsx
@@ -1,38 +1,38 @@
-import { useContext } from "react";
+//import { useContext } from "react";
 
-import Product  from "../../services/product";
-import { AuthContext } from "../authContext/index.jsx";
-import ActiveLink from "../ActiveLink"
+//import Product  from "../../services/product";
+//import { AuthContext } from "../authContext/index.jsx";
+//import ActiveLink from "../ActiveLink"
 
 
 function Header() {
-    const { signOut, user } = useContext(AuthContext);
-    const product = Product(); 
+//    const { signOut, user } = useContext(AuthContext);
+//    const product = Product(); 
 
-    async function logout() {
-        await signOut();
-    }
+//    async function logout() {
+//        await signOut();
+//    }
 
     return (
         <header className="Header">
-            <div className="Container">
-                <ActiveLink style={{width: '50%'}} href="/"  >
-                    <h1 className="Title">Portfolio - Northon Gracik</h1>
-                </ActiveLink>
-                <nav>
-                    {!user ?
-                        <>
-                            <a href="/login">Login</a>
-                        </>
-                        :
-                        <>
-                            <a href="/dashboard">{user.name}</a>
-                            <a onClick={logout}>Logout</a>
-                        </>
-                    }
-                    <a href="/checkout">{product.length}</a>
-                </nav>
-            </div>
+//            <div className="Container">
+//                <ActiveLink style={{width: '50%'}} href="/"  >
+//                    <h1 className="Title">Portfolio - Northon Gracik</h1>
+//                </ActiveLink>
+//                <nav>
+//                    {!user ?
+//                        <>
+//                            <a href="/login">Login</a>
+//                        </>
+//                        :
+//                        <>
+//                            <a href="/dashboard">{user.name}</a>
+//                            <a onClick={logout}>Logout</a>
+//                        </>
+//                    }
+//                    <a href="/checkout">{product.length}</a>
+//                </nav>
+//            </div>
         </header>
     )
 }
