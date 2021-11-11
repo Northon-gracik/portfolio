2d0b562cafe2ff659c1dd4a6f364c81c2e059a76
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-11-11 09:09:45 -0300

    Coment app

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,8 +1,8 @@
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+//import { Provider } from "react-redux";
+//import { PersistGate } from "redux-persist/integration/react";
 
-import { persistor, store } from "../src/store";
-import { AuthProvider } from '../src/components/authContext';
+//import { persistor, store } from "../src/store";
+//import { AuthProvider } from '../src/components/authContext';
 
 import '../styles/globals.css';
 import '../styles/index.css';
@@ -11,13 +11,13 @@ import '../styles/cart.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Provider store={store} >
-        <PersistGate loading={undefined} persistor={persistor}>
+//    <AuthProvider>
+//      <Provider store={store} >
+//        <PersistGate loading={undefined} persistor={persistor}>
           <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    </AuthProvider>
+//        </PersistGate>
+//      </Provider>
+//    </AuthProvider>
   )
 }
 
