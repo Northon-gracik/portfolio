623fceeb44a74e67b1d94ee678400c8295e87d2e
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-11-11 09:07:20 -0300

    Coment the index page

--- front/pages/index.jsx
+++ front/pages/index.jsx
@@ -1,47 +1,47 @@
-import Corner from "react-github-corner"
+//import Corner from "react-github-corner"
 
-import Header from "../src/components/Header";
-import FundoPrincipal from "../src/components/FundoPrincipal";
-import { getAllProducts } from "../src/services/apiGet.js";
-import { useState } from "react";
+//import Header from "../src/components/Header";
+//import FundoPrincipal from "../src/components/FundoPrincipal";
+//import { getAllProducts } from "../src/services/apiGet.js";
+//import { useState } from "react";
 
 function Home (props) {
   // const { dataProducts } = props
 
-  const [dataProducts, setDataProducts] = useState([]);
+//  const [dataProducts, setDataProducts] = useState([]);
 
-  getAllProducts().then( data => setDataProducts(data));
+//  getAllProducts().then( data => setDataProducts(data));
 
-  fetch("https://api.github.com/users/Northon-gracik").then( data => console.log(data));
+//  fetch("https://api.github.com/users/Northon-gracik").then( data => console.log(data));
 
   return (
     <div>
-      <Header />
-      <Corner octoColor="#E42044" />
-      <FundoPrincipal />
-      <div>
-        <h3>Mais vistos</h3>
-        <div className="card-box">
-          {dataProducts.map(data => {
-            return (
-              <ul key={data._id}>
-                <a href={`/produtos/${data._id}`}>
-                  <div className="card">
-                    <h3>{data.name}</h3>
-                    <p>{data.description}</p>
-                  </div>
-                </a>
-              </ul>
-            )
-          })}
-        </div>
-      </div>
-      {/* 
-
-      <footer>
-        <h3>Portifolio - Northon Gracik</h3>
-      </footer> 
-      */}
+//      <Header />
+//      <Corner octoColor="#E42044" />
+//      <FundoPrincipal />
+//      <div>
+//        <h3>Mais vistos</h3>
+//        <div className="card-box">
+//          {dataProducts.map(data => {
+//            return (
+//              <ul key={data._id}>
+//                <a href={`/produtos/${data._id}`}>
+//                  <div className="card">
+//                    <h3>{data.name}</h3>
+//                    <p>{data.description}</p>
+//                  </div>
+//                </a>
+//              </ul>
+//            )
+//          })}
+//        </div>
+//      </div>
+//      {/* 
+//
+//      <footer>
+//        <h3>Portifolio - Northon Gracik</h3>
+//      </footer> 
+//      */}
     </div>
   )
 }
